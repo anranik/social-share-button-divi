e9eb5b3000208c4f00e571a54b35788867111e5a
anranik <anrctg@gmail.com>
2025-04-13 22:22:32 +0600

    Initial commit from Create React App

---
+++ __plugin_name.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Social Share Button Divi
+Plugin URI:  https://owlpixel.com/social-share-button-divi
+Description: An divi extension to set social share on divi website
+Version:     1.0.0
+Author:      Owlpixel
+Author URI:  https://owlpixel.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: ssbd-social-share-button-divi
+Domain Path: /languages
+
+Social Share Button Divi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Social Share Button Divi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Social Share Button Divi. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+
+if ( ! function_exists( 'ssbd_initialize_extension' ) ):
+/**
+ * Creates the extension's main class instance.
+ *
+ * @since 1.0.0
+ */
+function ssbd_initialize_extension() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/SocialShareButtonDivi.php';
+}
+add_action( 'divi_extensions_init', 'ssbd_initialize_extension' );
+endif;
---
+++ includes/__PluginName.php
@@ -0,0 +1,46 @@
+<?php
+
+class SSBD_SocialShareButtonDivi extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'ssbd-social-share-button-divi';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'social-share-button-divi';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * SSBD_SocialShareButtonDivi constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'social-share-button-divi', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new SSBD_SocialShareButtonDivi;
